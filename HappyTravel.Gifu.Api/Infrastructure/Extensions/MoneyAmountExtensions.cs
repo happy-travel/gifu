e9272121320775d36449406f695aa5545ea55abe
Denis Nikolaev <denis.nikolaev@happytravel.com>
2021-05-27 12:36:11 +0500

    fixed converting money amount to amex format

--- HappyTravel.Gifu.Api/Infrastructure/Extensions/MoneyAmountExtensions.cs
+++ HappyTravel.Gifu.Api/Infrastructure/Extensions/MoneyAmountExtensions.cs
@@ -1,4 +1,5 @@
-﻿using System.Globalization;
+﻿using System;
+using System.Globalization;
 using HappyTravel.Money.Models;
 
 namespace HappyTravel.Gifu.Api.Infrastructure.Extensions
@@ -6,6 +7,10 @@ namespace HappyTravel.Gifu.Api.Infrastructure.Extensions
     public static class MoneyAmountExtensions
     {
         public static string ToAmExFormat(this MoneyAmount moneyAmount)
-            => (moneyAmount.Amount * 100).ToString(CultureInfo.InvariantCulture);
+        {
+            var (amount, _) = moneyAmount;
+            int count = BitConverter.GetBytes(decimal.GetBits(amount)[3])[2];
+            return ((int)(amount * (int)Math.Pow(10, count))).ToString(CultureInfo.InvariantCulture);
+        }
     }
 }
