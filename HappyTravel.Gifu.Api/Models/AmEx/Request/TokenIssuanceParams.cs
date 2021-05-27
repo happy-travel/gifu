7dafd226fb0732204d21e5492a08838dd4afdd80
Denis Nikolaev <denis.nikolaev@happytravel.com>
2021-05-27 13:05:08 +0500

    added billing account id property

--- HappyTravel.Gifu.Api/Models/AmEx/Request/TokenIssuanceParams.cs
+++ HappyTravel.Gifu.Api/Models/AmEx/Request/TokenIssuanceParams.cs
@@ -4,6 +4,9 @@ namespace HappyTravel.Gifu.Api.Models.AmEx.Request
 {
     public readonly struct TokenIssuanceParams
     {
+        [JsonPropertyName("billing_account_id")]
+        public string BillingAccountId { get; init; }
+        
         [JsonPropertyName("token_details")]
         public TokenDetails TokenDetails { get; init; }
     }
--- HappyTravel.Gifu.Api/Services/VccService.cs
+++ HappyTravel.Gifu.Api/Services/VccService.cs
@@ -60,6 +60,7 @@ namespace HappyTravel.Gifu.Api.Services
                 {
                     TokenIssuanceParams = new TokenIssuanceParams
                     {
+                        BillingAccountId = string.Empty, // TODO: set account id for currency in request
                         TokenDetails = new TokenDetails
                         {
                             TokenReferenceId = request.ReferenceCode,
