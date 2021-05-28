da2b682f3f02aa755a37a80e722ecbc3c39d0b63
Denis Nikolaev <denis.nikolaev@happytravel.com>
2021-05-28 14:15:35 +0500

    fixed conversion, added tests

--- HappyTravel.Gifu.Api/Infrastructure/Extensions/MoneyAmountExtensions.cs
+++ HappyTravel.Gifu.Api/Infrastructure/Extensions/MoneyAmountExtensions.cs
@@ -10,8 +10,9 @@ namespace HappyTravel.Gifu.Api.Infrastructure.Extensions
     {
         public static string ToAmExFormat(this MoneyAmount moneyAmount)
         {
+            moneyAmount = MoneyRounder.Ceil(moneyAmount);
             var amount = moneyAmount.Amount * (int) Math.Pow(10, moneyAmount.Currency.GetDecimalDigitsCount());
-            return MoneyRounder.Ceil(amount, moneyAmount.Currency).ToString(CultureInfo.InvariantCulture);
+            return ((int)amount).ToString(CultureInfo.InvariantCulture);
         }
     }
 }
---
+++ HappyTravel.Gifu.UnitTests/MoneyAmountExtensionsTests.cs
@@ -0,0 +1,28 @@
+﻿using HappyTravel.Gifu.Api.Infrastructure.Extensions;
+using HappyTravel.Money.Enums;
+using HappyTravel.Money.Models;
+using Xunit;
+
+namespace HappyTravel.Gifu.Tests
+{
+    public class MoneyAmountExtensionsTests
+    {
+        [Theory]
+        [InlineData(0.001, Currencies.USD, "0")]
+        [InlineData(0.01, Currencies.USD, "1")]
+        [InlineData(0.1, Currencies.USD, "10")]
+        [InlineData(1, Currencies.USD, "100")]
+        [InlineData(1.01, Currencies.USD, "101")]
+        [InlineData(1.1, Currencies.USD, "110")]
+        [InlineData(1.11, Currencies.USD, "111")]
+        [InlineData(1.111, Currencies.USD, "111")]
+        [InlineData(1.1111, Currencies.USD, "111")]
+        private void Convert_to_amex_format_should_be_equal(decimal amount, Currencies currency, string result)
+        {
+            var moneyAmount = new MoneyAmount(amount, currency);
+            var value = moneyAmount.ToAmExFormat();
+            
+            Assert.Equal(result, value);
+        }
+    }
+}
